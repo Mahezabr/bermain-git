a956ce7c5ee998f19223e2b76eb3b0b1e87f4414
mahezabr <mahezabr99@gmail.com>
2019-10-20 14:08:19 +0700

    revisi

--- hacktoberfest.py
+++ hacktoberfest.py
@@ -1,5 +1,8 @@
 
-print("HACKTOBERFEST");
-print("-------------------------------");
-print("");
-print("My Name is Maheza");
+print("HACKTOBERFEST")
+print("-------------------------------")
+print("")
+print("My Name is Maheza")
+print("I was born in Semarang")
+print("I am a student of Dian Nuswantoro University")
+print("My Favorite food are Indonesian food, Chinese Food ,and Japanese Food")
